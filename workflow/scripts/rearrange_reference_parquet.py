5ac14a318ad55afbed4c27c0f7a39e52ec28f85d
Carolyn Turcotte <carolyn.turcotte@unc.edu>
2024-08-23 15:44:43 -0400

    parquet rearrangement works

--- workflow/scripts/rearrange_reference_parquet.py
+++ workflow/scripts/rearrange_reference_parquet.py
@@ -1,139 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Connect to the SQL database.
+
+# In[ ]:
+
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Import args from snakemake into iPython')
+
+parser.add_argument('ref_parent_file')
+parser.add_argument('alt_parent_file')
+parser.add_argument('ref_parent_name')
+parser.add_argument('alt_parent_name')
+parser.add_argument('ref_qual_cutoff')
+parser.add_argument('ref_depth_cutoff')
+parser.add_argument('ref_out_file')
+parser.add_argument('db')
+
+args = parser.parse_args()
+
+ref_parent_file = args.ref_parent_file
+alt_parent_file = args.alt_parent_file
+ref_qual_cutoff = args.ref_qual_cutoff
+ref_depth_cutoff = args.ref_depth_cutoff
+ref_out_file = args.ref_out_file
+os.environ['DATABASE_URL'] = f"duckdb://default.db"
+
+
+# In[ ]:
+
+
 import duckdb
+import pandas as pd
+import ipywidgets
+
+# %config SqlMagic.autopandas = True
+# %config SqlMagic.feedback = False
+# %config SqlMagic.displaycon = False
+
+get_ipython().run_line_magic('load_ext', 'sql')
+
+# get_ipython().run_line_magic('config', 'SqlMagic.autopandas = False')
+# get_ipython().run_line_magic('config', 'SqlMagic.feedback = False')
+# get_ipython().run_line_magic('config', 'SqlMagic.displaycon = False')
+
+conn = duckdb.connect()
+get_ipython().run_line_magic('sql', 'conn --alias duckdb')
 
 
-if __name__ == "__main__":
-    # tuples to avoid SQL injection!
-    ref_parent_file = snakemake.input['ref']
-    alt_parent_file = snakemake.input['alt']
-    ref_parent_name = snakemake.config['ref_parent']
-    alt_parent_name = snakemake.config['alt_parent']
-    ref_qual_cutoff = snakemake.params['min_qual']
-    ref_depth_cutoff = snakemake.params['min_depth']
-    ref_out_file = snakemake.output[0]
+# In[43]:
 
-    con = duckdb.connect(snakemake.output['db'], config = {'threads': snakemake.threads, 'memory_limit': (f"{snakemake.resources['mem_mb']/1000 - 2}G")})
-    con.sql("SET preserve_insertion_order = false")
-    con.execute("""
-        SELECT
-            sample,
-            chromosome,
-            CAST(position AS INTEGER) AS int_pos,
-            quality,
-            genotype,
-            depth,
-            allele_depth,
-            UPPER(reference) AS up_reference,
-            UPPER(variant) AS up_variant,
-            (CASE WHEN variant='.' THEN reference ELSE UPPER(variant) END) AS mod_variant
-        FROM
-            read_parquet([?, ?])""", [ref_parent_file, alt_parent_file])
-    parents = con.fetchall()
-    con.sql(
-        """CREATE
-        OR REPLACE VIEW check_unique_variants AS
-        SELECT
-            chromosome,
-            int_pos,
-            COUNT(up_variant) AS n_variants
-        FROM
-            parents
-        GROUP BY
-            chromosome,
-            int_pos
-        HAVING
-            n_variants >= 2"""
-        )
-    con.execute("""
-        SELECT
-            sample,
-            parents.chromosome,
-            parents.int_pos,
-            up_reference AS reference,
-            mod_variant,
-            quality,
-            genotype,
-            depth,
-            allele_depth
-        FROM
-            parents
-            INNER JOIN check_unique_variants ON (
-                parents.chromosome = check_unique_variants.chromosome
-                AND parents.int_pos = check_unique_variants.int_pos
-            )
-        WHERE
-            genotype != '0/1'
-            AND quality > ?
-            AND LENGTH (mod_variant) <= 1
-            AND LENGTH (up_reference) <= 1
-            AND depth > ?""", [ref_qual_cutoff, ref_depth_cutoff])
-    temp_ref = con.fetchall()
-    con.execute("""
-        SELECT
-            sample,
-            chromosome,
-            int_pos,
-            mod_variant AS ref_allele
-        FROM
-            temp_ref
-        WHERE
-            sample = ?""", [ref_parent_name])
-    ref_parent = con.fetchall()
-    con.execute("""
-        SELECT
-            sample,
-            chromosome,
-            int_pos,
-            mod_variant AS alt_allele
-        FROM
-            temp_ref
-        WHERE
-            sample = ?""", [alt_parent_name])
-    alt_parent = con.fetchall()
-    con.sql(
-        """CREATE
-        OR REPLACE TABLE ref AS
-        SELECT
-            *
-        FROM
-            ref_parent
-            INNER JOIN alt_parent ON (
-                ref_parent.chromosome = alt_parent.chromosome
-                AND ref_parent.int_pos = alt_parent.int_pos
-            )"""
-        )
-    con.sql(
-        """CREATE
-        OR REPLACE TABLE refs_unique AS
-        SELECT
-            *
-        FROM
-            ref
-        WHERE
-            ref_allele != alt_allele"""
-        )
-    con.execute(
-        """
-        SELECT
-            chromosome,
-            int_pos AS position,
-            ref_allele AS reference,
-            alt_allele AS variant
-        FROM
-                ref
-        """
-    )
-    ref_final = con.fetchall()
-    ref_final.write_parquet(ref_out_file)
-    # con.execute("""COPY (
-            # SELECT
-                # chromosome,
-                # int_pos AS position,
-                # ref_allele AS reference,
-                # alt_allele AS variant
-            # FROM
-                # ref
-            # )
-            # TO
-                # ?
-            # (FORMAT 'parquet')""", [ref_out_file])
-    con.close()
+
+get_ipython().run_cell_magic('sql', '', 'SET preserve_insertion_order = false;\n')
+
+
+# Define the locations of the references and samples, and quality/depth cutoffs.
+
+# In[44]:
+
+
+# ref_parent_file = snakemake.input['ref']
+# alt_parent_file = snakemake.input['alt']
+# ref_parent_name = snakemake.config['ref_parent']
+# alt_parent_name = snakemake.config['alt_parent']
+
+# ref_qual_cutoff = snakemake.params['min_qual']
+# ref_depth_cutoff = snakemake.params['min_depth']
+
+# ref_out_file = snakemake.output[0]
+
+
+# Import the reference files as a table. 
+
+# In[45]:
+
+
+get_ipython().run_cell_magic('sql', '', "CREATE\nOR REPLACE VIEW parents AS\nSELECT\n    sample,\n    chromosome,\n    CAST(position AS INTEGER) AS int_pos,\n    quality,\n    genotype,\n    depth,\n    allele_depth,\n    UPPER(reference) AS up_reference,\n    UPPER(variant) AS up_variant,\n    (CASE WHEN variant='.' THEN reference ELSE UPPER(variant) END) AS mod_variant\nFROM\n    read_parquet(['{{ref_parent_file}}', '{{alt_parent_file}}']);\n")
+
+
+# Isolate sites where there are 2 variants (meaning one parent is different from the other).
+
+# In[46]:
+
+
+get_ipython().run_cell_magic('sql', '', 'CREATE\nOR REPLACE VIEW check_unique_variants AS\nSELECT\n    chromosome,\n    int_pos,\n    COUNT(up_variant) AS n_variants\nFROM\n    parents\nGROUP BY\n    chromosome,\n    int_pos\nHAVING\n    n_variants >= 2;\n')
+
+
+# Create new reference from the reference parent.
+
+# In[47]:
+
+
+get_ipython().run_cell_magic('sql', '', "CREATE\nOR REPLACE VIEW temp_ref AS\nSELECT\n    sample,\n    parents.chromosome,\n    parents.int_pos,\n    up_reference AS reference,\n    mod_variant,\n    quality,\n    genotype,\n    depth,\n    allele_depth\nFROM\n    parents\n    INNER JOIN check_unique_variants ON (\n        parents.chromosome = check_unique_variants.chromosome\n        AND parents.int_pos = check_unique_variants.int_pos\n    )\nWHERE\n    genotype != '0/1'\n    AND quality > '{{ref_qual_cutoff}}'\n    AND LENGTH (mod_variant) <= 1\n    AND LENGTH (up_reference) <= 1\n    AND depth > '{{ref_depth_cutoff}}';\n")
+
+
+# In[48]:
+
+
+get_ipython().run_cell_magic('sql', '', "CREATE\nOR REPLACE VIEW ref_parent AS\nSELECT\n    sample,\n    chromosome,\n    int_pos,\n    mod_variant AS ref_allele\nFROM\n    temp_ref\nWHERE\n    sample = '{{ref_parent_name}}';\n\nCREATE\nOR REPLACE VIEW alt_parent AS\nSELECT\n    sample,\n    chromosome,\n    int_pos,\n    mod_variant AS alt_allele\nFROM\n    temp_ref\nWHERE\n    sample = '{{alt_parent_name}}';\n")
+
+
+# In[49]:
+
+
+get_ipython().run_cell_magic('sql', '', 'CREATE\nOR REPLACE TABLE ref AS\nSELECT\n    *\nFROM\n    ref_parent\n    INNER JOIN alt_parent ON (\n        ref_parent.chromosome = alt_parent.chromosome\n        AND ref_parent.int_pos = alt_parent.int_pos\n    );\n')
+
+
+# In[50]:
+
+
+get_ipython().run_cell_magic('sql', '', 'CREATE\nOR REPLACE TABLE refs_unique AS\nSELECT\n    *\nFROM\n    ref\nWHERE\n    ref_allele != alt_allele;\n')
+
+
+# In[58]:
+
+
+get_ipython().run_cell_magic('sql', '', "COPY (SELECT chromosome, int_pos AS position, ref_allele AS reference, alt_allele AS variant FROM refs_unique)\nTO '{{ref_out_file}}'\n(FORMAT 'parquet');\n")
+
--- workflow/scripts/rearrange_sample_parquet.py
+++ workflow/scripts/rearrange_sample_parquet.py
@@ -1,109 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Import args from snakemake into iPython')
+
+parser.add_argument('sample_name')
+parser.add_argument('ref_file')
+parser.add_argument('sample_file')
+parser.add_argument('sample_out_file')
+
+args = parser.parse_args()
+
+sample_name = args.sample_name
+ref_file = args.ref_file
+sample_file = args.sample_file
+sample_out_file = args.sample_out_file
+os.environ['DATABASE_URL'] = f"duckdb://default.db"
+
 import duckdb
+import pandas as pd
+import ipywidgets
+
+conn = duckdb.connect(f'{sample_name}.db')
+
+get_ipython().run_line_magic('load_ext', 'sql')
+get_ipython().run_line_magic('sql', 'conn --alias duckdb')
 
 
-if __name__ == "__main__":
-    ref = snakemake.input['ref']
-    smp = snakemake.input['smp']
-    out_file = snakemake.output[0]
+# In[ ]:
 
-    con = duckdb.connect(snakemake.output['db'], config = {
-        'threads': snakemake.threads,
-        'memory_limit': (
-            f"{snakemake.resources['mem_mb']/1000 - 2}G"
-            )
-        })
-    ref_pqt = con.read_parquet(ref)
-    smp_pqt = con.read_parquet(smp)
-    con.sql(
-    """CREATE
-    OR REPLACE VIEW ref AS
-    SELECT
-        *
-    FROM
-        ref_pqt
-    """
-    )
-    con.sql(
-        """CREATE
-        OR REPLACE VIEW temp_vcfs AS
-        SELECT
-            *
-        FROM
-            smp_pqt"""
-    )
-    con.sql(
-        """CREATE
-        OR REPLACE VIEW vcfs AS
-        SELECT
-            sample,
-            chromosome,
-            CAST(position AS INTEGER) AS int_pos,
-            reference,
-            variant,
-            quality,
-            genotype,
-            depth,
-            allele_depth,
-            (
-                CASE
-                    WHEN temp_vcfs.variant = '.' THEN UPPER(temp_vcfs.reference)
-                    ELSE UPPER(temp_vcfs.variant)
-                END
-            ) AS new_variant
-        FROM
-            temp_vcfs"""
-    )
-    con.sql(
-    """CREATE
-    OR REPLACE VIEW samples_rearranged AS
-    SELECT
-        *,
-        (
-            CASE
-                WHEN ref_allele = new_variant THEN '.'
-                ELSE new_variant
-            END
-        ) AS var_adjusted
-    FROM
-        vcfs
-        INNER JOIN refs ON vcfs.chromosome = refs.chromosome
-        AND vcfs.int_pos = refs.int_pos
-    WHERE
-        new_variant = ref_allele
-        OR new_variant = alt_allele"""
-    )
-    con.sql("SET reserve_insertion_order = true")
-    final_pqt = con.sql(
-        """CREATE
-        OR REPLACE TABLE final_samples AS
-        SELECT 
-            string_split(sample, '-')[1] AS condition,
-            string_split(sample, '-')[2] AS sample_type,
-            string_split(sample, '-')[3] AS sample_num,
-            UPPER(ref_allele) AS reference,
-            UPPER(var_adjusted) AS variant,
-            chromosome,
-            int_pos AS position,
-            CAST(string_split(allele_depth, ',')[1] AS INTEGER) AS ref_reads,
-            CAST(string_split(allele_depth, ',')[2] AS INTEGER) AS variant_reads,
-            quality AS QUAL,
-            (CASE WHEN var_adjusted='.' AND genotype='1/1' THEN '0/0' WHEN var_adjusted !='.' AND genotype='0/0' THEN '1/1' ELSE genotype END) AS GT,
-            depth AS DP
-        FROM
-            samples_rearranged
-        ORDER BY
-            sample_num, chromosome, position"""
-        )
-    final_pqt.write_parquet(out_file) 
-    con.close()
-    # con.sql(
-    #     """COPY (
-    #         SELECT
-    #             *
-    #         FROM
-    #             final_samples
-    #         )
-    #     TO
-    #         sample_out_file
-    #     (FORMAT 'parquet')"""
-    # )
+
+get_ipython().run_cell_magic('sql', '', '\nSET preserve_insertion_order = false;\n')
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "\nCREATE\nOR REPLACE VIEW temp_vcfs AS\nSELECT\n    *\nFROM\n    read_parquet('{{sample_file}}');\n")
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "\nCREATE\nOR REPLACE VIEW vcfs AS\nSELECT\n    sample,\n    chromosome,\n    CAST(position AS INTEGER) AS int_pos,\n    reference AS old_ref,\n    variant AS old_variant,\n    quality,\n    genotype,\n    depth,\n    allele_depth,\n    (\n        CASE\n            WHEN temp_vcfs.variant = '.' THEN UPPER(temp_vcfs.reference)\n            ELSE UPPER(temp_vcfs.variant)\n        END\n    ) AS new_variant\nFROM\n    temp_vcfs;\n")
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "CREATE\nOR REPLACE TABLE refs AS\nSELECT\n    *\nFROM\n    read_parquet('{{ref_file}}');\n")
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "\nCREATE\nOR REPLACE VIEW samples_rearranged AS\nSELECT\n    *,\n    (\n        CASE\n            WHEN reference = new_variant THEN '.'\n            ELSE new_variant\n        END\n    ) AS var_adjusted\nFROM\n    vcfs\n    INNER JOIN refs ON vcfs.chromosome = refs.chromosome\n    AND vcfs.int_pos = refs.position\nWHERE\n    new_variant = reference\n    OR new_variant = variant;\n")
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "SET preserve_insertion_order = true;\nCREATE\nOR REPLACE TABLE final_samples AS\nSELECT \n    string_split(sample, '-')[1] AS condition,\n    string_split(sample, '-')[2] AS sample_type,\n    string_split(sample, '-')[3] AS sample_num,\n    UPPER(reference) AS reference,\n    UPPER(var_adjusted) AS variant,\n    chromosome,\n    int_pos AS position,\n    CAST(string_split(allele_depth, ',')[1] AS INTEGER) AS ref_reads,\n    CAST(string_split(allele_depth, ',')[2] AS INTEGER) AS variant_reads,\n    quality AS QUAL,\n    (CASE WHEN var_adjusted='.' AND genotype='1/1' THEN '0/0' WHEN var_adjusted !='.' AND genotype='0/0' THEN '1/1' ELSE genotype END) AS GT,\n    depth AS DP\nFROM\n    samples_rearranged\nORDER BY\n    sample_num, chromosome, position;\n")
+
+
+# In[ ]:
+
+
+get_ipython().run_cell_magic('sql', '', "\nCOPY (SELECT * FROM final_samples)\nTO '{{sample_out_file}}'\n(FORMAT 'parquet');\n")
+
